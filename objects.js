e8c2ccc17a51e0171bab596d15a46f1bd00cdda4
wley3337@gmail.com <wley3337@gmail.com>
2018-03-21 23:40:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = {};
 
 //create a function updatePlaylist that takes 3 peramiters: playlist, artist name, and song title should add the values to playlist, should return the playlist. 
 
-function updatePlaylist (oldPlaylist, artistName, songTitle){
-  var playlist = {oldPlaylist, [artistName: songTitle]};
-  return playlist;
+function updatePlaylist (playlist, artistName, songTitle){
+  return Object.assign (playlist, { [artistName]: songTitle});
+  
 }
 
 //create a function removeFromPlaylist that accepst two arguments playlist and artists name. delete the key value pair from the playlist and return the updated playlist
