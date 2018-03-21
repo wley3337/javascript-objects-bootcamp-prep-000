0ef7eaf91dff83e3e9703c3ba02fc626a1100da5
wley3337@gmail.com <wley3337@gmail.com>
2018-03-21 23:53:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = { Envogue : 'Free Your Mind' };
 //create a function updatePlaylist that takes 3 peramiters: playlist, artist name, and song title should add the values to playlist, should return the playlist. 
 
 function updatePlaylist (playlist, artistName, songTitle){
-  return Object.assign (playlist, { [artistName]: songTitle});
+  return Object.assign (playlist, { [artistName]: 'songTitle'});
 }
 
 //create a function removeFromPlaylist that accepst two arguments playlist and artists name. delete the key value pair from the playlist and return the updated playlist
